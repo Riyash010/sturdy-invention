3188f6dec98b7ec0a9ace2037f9c9434fca9a96c
Riyash010 <reeyash14@gmail.com>
2026-03-30 01:01:53 +0000

    Type in vite con fig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   base: process.env.VERCEL ? '/' : '/sturdy-inventio/',
+  base: process.env.VERCEL ? '/' : '/sturdy-invention/',
 })
